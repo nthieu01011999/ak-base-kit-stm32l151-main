dbbe4ce54ea004aadd39c094b127284033b422cc
nthieu01011999 <nthieu01011999@gmail.com>
2023-09-05 17:37:59 +0700

    delete arduino

--- application/sources/networks/Makefile.mk
+++ application/sources/networks/Makefile.mk
@@ -1,5 +1,5 @@
 ifeq ($(IF_NETWORK_NRF24_OPTION),-DIF_NETWORK_NRF24_EN)
-include sources/networks/rf_protocols/Makefile.mk
+#include sources/networks/rf_protocols/Makefile.mk
 endif
 
 ifeq ($(IF_LINK_OPTION),-DIF_LINK_UART_EN)
@@ -7,7 +7,7 @@ include sources/networks/net/link/Makefile.mk
 endif
 
 ifeq ($(TASK_ZIGBEE_OPTION),-DTASK_ZIGBEE_EN)
-include sources/networks/ArduinoZigBee/Makefile.mk
+#include sources/networks/ArduinoZigBee/Makefile.mk
 endif
 
 ifeq ($(TASK_MBMASTER_OPTION),-DTASK_MBMASTER_EN)
